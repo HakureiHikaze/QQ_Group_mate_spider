149ff773a073cb06c6443951c133d32d8cefaeaa
HakureiHikaze <wgyaaa123@gmail.com>
2021-04-12 19:29:21 +0800

    Corrected some problems.

--- NewSpider.py
+++ NewSpider.py
@@ -7,6 +7,9 @@ from selenium.common.exceptions import NoSuchFrameException, NoSuchElementExcept
 
 class newSpider:
     def __init__(self, driver: webdriver):
+        """
+        :param driver: WebDriver from selenium
+        """
         self.currentGroup = ""
         self.currentGroupName = ""
         self.currentFileName = ""
@@ -19,7 +22,7 @@ class newSpider:
         while i:
             try:
                 self.driver.switch_to.frame("login_frame")
-                login = self.driver.find_element_by_class_name("face")  # 获取头像登录按键
+                login = self.driver.find_element_by_class_name("face")  # get face to login
                 login.click()
                 time.sleep(3)
                 li_list = self.find_ul_element()
@@ -39,27 +42,26 @@ class newSpider:
         return li_list
 
     def scroll(self):
-        # 滚动条下拉
+        # scroll
         js = "var q=document.documentElement.scrollTop=100000"
         return self.driver.execute_script(js)
 
     def getTbodyList(self):
-        print("getTbodyList()获取Tbody列表")
+        print("getTbodyList() get Tbody list")
         return self.driver.find_elements_by_xpath('//div[@class="group-memeber"]//tbody[contains(@class,"list")]')
 
     def parseTbody(self, html):
         """
-        解析tbody里面的内容，一个tbody里面有多个成员，
-        解析完成后，返回成员基本情况的列表
-        :param html:
-        :return:
+        Parse tbody html to get raw member list.
+        :param html: input Tbody HTML
+        :return: parsed member list
         """
-        print("parseTbody()解析Tbody")
+        print("parseTbody() parse Tbody")
         memberLists = []
         for each in html:
             memberList = each.find_elements_by_xpath('tr[contains(@class,"mb mb")]')
             memberLists += memberList
-        # print("memberLists长度为：{}".format(len(memberLists)))
+        # print("memberLists length：{}".format(len(memberLists)))
         memberLists_data = []
         for each in memberLists:
             memberLists_data.append(self.parseMember(each))
@@ -67,13 +69,13 @@ class newSpider:
 
     def parseMember(self, mb):
         """
-        解析每个人各项描述，以逗号隔开，返回一个成员的基本情况
-        :param mb:
-        :return:
+        Parse group members' info.
+        :param mb: raw member info
+        :return: member info as string
         """
-        print("parseMember()解析成员信息")
+        print("parseMember() parse member info")
         td = mb.find_elements_by_xpath('td')
-        # print("td长度为：{}".format(len(td)))
+        # print("td length：{}".format(len(td)))
         nickname = td[2].find_element_by_xpath('span').text.strip()
         nicknameInGroup = td[3].find_element_by_xpath('span').text.strip()
         qq = td[4].text.strip()
@@ -84,24 +86,22 @@ class newSpider:
         # sex = td[5].text.strip()
         # qqAge = td[6].text.strip()
         # joinTime = td[7].text.strip()
-        # lastTime = td[8].text.strip()
+        # lastTalkTime = td[8].text.strip()
 
-        # 1序号，2昵称，3群名片，4QQ号，5性别，6Q龄，7入群时间，8最后发言时间
-
-        member = (self.currentGroupName + "," + qq + "," + nickname + "," + nicknameInGroup)
+        member = self.currentGroupName + "," + qq + "," + nickname + "," + nicknameInGroup
         print(member)
         return member
 
     def parseAndWrite(self, tbody):
         """
-        解析HTML中的tbody，解析完成后写入到本地文件
+        parse tbody in HTML and write
         :param tbody:
         :return:
         """
-        print("parseAndWrite()写入文件")
+        print("parseAndWrite() written")
         memberList = self.parseTbody(tbody)
-        with open(self.currentFileName, 'a+', encoding="utf-8") as f:  # 如果分析群名带windows禁止作为文件名的字符的群，
-            for each in memberList:  # 则会出错，但不会引发异常。如出错，将groupName改成qqgroup
+        with open(self.currentFileName, 'a+', encoding="utf-8") as f:
+            for each in memberList:
                 f.write(str(each) + "\n")
 
     def parse(self):
@@ -111,19 +111,27 @@ class newSpider:
         self.currentGroupName = self.driver.find_element_by_id("groupTit").text.strip()
         self.currentGroup = re.findall(r'\([0-9]*\)', self.currentGroupName)[0][1:-1]
         self.currentFileName = self.currentGroup + ".csv"
-        print("群人数: " + str(qqNum))
+        print("Group size: " + str(qqNum))
         while currentNum != qqNum:
             currentNum = len(self.driver.find_elements_by_xpath('//*[@id="groupMember"]//td[contains(@class,"td-no")]'))
-            # 向下滚动屏幕，直到底部
+            # Scroll the screen
             self.scroll()
-            # 每次滚动休息1秒
+            # wait
             time.sleep(1)
             tList = self.getTbodyList()
             self.parseAndWrite(tList[prelen:])
-            prelen = len(tList)  # 更新tbody列表的长度
+            prelen = len(tList)  # update length of tList
 
     def exe(self):
-        self.parse()
+        i = True
+        while i:
+            try:
+                self.parse()
+                i = False
+            except Exception:  # I don't know which exception may be triggered. The same below.
+                self.driver.navigate().refresh()
+                os.remove(self.currentFileName)
+
         li_list = self.find_ul_element()
         for j in range(1, len(li_list)):
             i = True
@@ -143,7 +151,7 @@ class newSpider:
 
 def main():
     driver = webdriver.Chrome()
-    # webdriver.ChromeOptions().add_argument("headless")
+    # webdriver.ChromeOptions().add_argument("headless") # todo: Uncomment this line will run silently.
     spider = newSpider(driver)
     spider.exe()
     print()
